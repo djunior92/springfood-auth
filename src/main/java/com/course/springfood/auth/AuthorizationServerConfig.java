e66484545ccbe3c335ad41e7df4d4c6163e953b2
djunior92 <juninho_2509@hotmail.com>
2022-01-03 19:52:33 -0300

    Extraindo a chave pública

--- src/main/java/com/course/springfood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/course/springfood/auth/AuthorizationServerConfig.java
@@ -74,6 +74,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
         security.checkTokenAccess("permitAll()")
+                .tokenKeyAccess("permitAll()")
                 .allowFormAuthenticationForClients();
     }
 
